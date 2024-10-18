ac636eb8ccce9e5fc552951b88c61079e5a6666d
adam-THS <adam@twohourssleep.com>
2024-10-18 13:26:50 +0100

    .

--- assets/primary/js/header.js
+++ assets/primary/js/header.js
@@ -1,6 +1,9 @@
 const mobile_toggle_open = document.getElementsByClassName('icon-hamburger');
 const mobile_toggle_close = document.getElementsByClassName('icon-close');
 
+console.log('hello');
+
+
 mobile_toggle_open.onclick = function () {
   const mobile_toggle = document.getElementsByClassName('mobile-toggle');
   mobile_toggle.classList.toggle('active');
