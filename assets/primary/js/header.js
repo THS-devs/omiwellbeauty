55a96ed4d33625091c097090ba9a7385c6eb9e87
adam-THS <adam@twohourssleep.com>
2024-10-21 13:27:44 +0100

    .

--- assets/primary/js/header.js
+++ assets/primary/js/header.js
@@ -1,13 +1,16 @@
 const mobile_toggle_open = document.querySelector('.icon-hamburger');
 const mobile_toggle_close = document.querySelector('.icon-close');
+const mobile_menu_drawer = document.querySelector('.mobile-menu-drawer');
 
 
 mobile_toggle_open.addEventListener('click', function (e) {
   const mobile_toggle = document.querySelector('.mobile-toggle');
   mobile_toggle.classList.add('active');
+  mobile_menu_drawer.classList.add('active');
 });
 mobile_toggle_close.addEventListener('click', function (e) {
   const mobile_toggle = document.querySelector('.mobile-toggle');
   mobile_toggle.classList.remove('active');
+  mobile_menu_drawer.classList.remove('active');
 });
 
