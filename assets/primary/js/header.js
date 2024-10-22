985033b6b21fd1a35a3aa865ba96bde78ebb12d7
adam-THS <adam@twohourssleep.com>
2024-10-22 12:42:51 +0100

    .

--- assets/primary/js/header.js
+++ assets/primary/js/header.js
@@ -8,6 +8,7 @@ mobile_toggle_open.addEventListener('click', function (e) {
   const mobile_toggle = document.querySelector('.mobile-toggle');
   mobile_toggle.classList.add('active');
   mobile_menu_drawer.classList.add('active');
+  console.log('hello');
 });
 
 mobile_toggle_close.addEventListener('click', function (e) {
