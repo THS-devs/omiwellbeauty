91bb7747693a3472e068852feeded827c2d29972
adam-THS <adam@twohourssleep.com>
2024-10-22 12:36:19 +0100

    .

---
+++ assets/primary/js/main.js
@@ -0,0 +1,23 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // Autoplay settings
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+
+  // Responsive breakpoints
+  breakpoints: {
+    1024: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 1,
+    },
+    480: {
+      slidesPerView: 1,
+    },
+  },
+}); 
